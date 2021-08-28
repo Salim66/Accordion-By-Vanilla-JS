62f53d1126002c45d5bd53b05db478dae93f4235
Salim66 <salimhasanriad8@gmail.com>
2021-08-28 08:32:48 +0600

    JavaScript functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
 // grab everything we need
-
+const accordion = document.querySelector('.accordion');
+const items     = document.querySelectorAll('li');
+const questions = document.querySelectorAll('.question');
 
 
 // define all functions w'll nedd
+function toggleAccordion(){
+    let thisItem = this.parentNode;
 
+    items.forEach(item => {
+        if(thisItem == item){
+            thisItem.classList.toggle('open');
+            return;
+        }
+        item.classList.remove('open');
+    });
+}
 
 
 // add event listeners
+questions.forEach(question => question.addEventListener('click', toggleAccordion));
